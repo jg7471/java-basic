69b006175ca15b56b0c97947ae9bc09feb48561a
jg <iamjgl@naver.com>
2024-02-29 15:43:09 +0900

    コミット-6 240229

--- src/etc_JG/exception/A05api/lang/obj/MainClass.java
+++ src/etc_JG/exception/A05api/lang/obj/MainClass.java
@@ -1,11 +1,14 @@
 package etc_JG.exception.A05api.lang.obj;
 
+import java.util.HashSet;
+//toSting 많이 씀 equal+hash 가끔 clone 가끔
+
 public class MainClass {
 
     public static void main(String[] args) { //메인 메서드란??@@@
 
-        Person kim = new Person("김춘식",30); //@@@언급한거 인강
-        Person hong = new Person("홍길동",25);
+        Person kim = new Person("홍길동",25); //@@@언급한거 인강
+        Person hong = new Person("홍길당",25); //hash는 이름 같아야 함
 
         //String name = "박영희";
         String name1 = new String("박영희");
@@ -13,10 +16,51 @@ public class MainClass {
 
         //객체 타입의 변수를 출력하거나 값을 얻을 때는
         //뒤에 자동으로 .toString()이 붙어서 진행됨.(Object가 물려주는 메서드)
-        System.out.println(kim.toString()); //주소 출력 //object
+        //System.out.println(kim.toString()); //주소 출력 //object
+        System.out.println(kim);
         System.out.println(hong);
         System.out.println(name1);
 
+        if(kim.equals(hong)){
+            System.out.println("이름과 나이가 같네요");
+        }else{
+            System.out.println("이름 or 나이 or  둘 다 다를 수도");
+        }
+        //object 최상위 부모 메서드 준비함 : 자식들이 오버라이딩해서 커스텀해서 사용하라
+        //kim.equals(hong); //kim == hong 의미
 
+        /*
+        System.out.println(kim.hashCode()); //자바에서는 기본적으로 객체와 객체는 동등하다 : 객체는 다른데 주소는 동일
+        System.out.println(hong.hashCode()); //주소값 다르면 ? 다른 객체 ,
+        //이름 = 나이 같으면 동일한 객체로 하자 : equals 메소드 자성할 때 1) hashCode 있어야 동일한 주소값 얻을 수 있음 : hashSet
+        Set<Person> set = new HashSet<>(); // 주소값 같을 시 중복 객체값 제거하고 출력
+        set.add(kim);
+        set.add(hong);
+        System.out.println(set);
+           */
+
+        // 문법 밑줄 쳐져있으 사라질 가능성 多
+        /* finalize는 gc가 호출되는 순서를 보장하지 않기에 권장X : 쓸 일 없음 (deprecated)
+        hong = null; // @@@??인강
+        kim = null;
+        System.gc(); //가비치 컬렉터 호출 (부른다고 바로 안 올수도...(우선순위대로)) : 사용권장 X
+        */
+
+        Person park = new Person("박복제인간",100);
+        //Person clonePeson = park; // park-> clonePerson 주소값 넘긴 것 뿐임 : 객체복사 X
+
+        try {
+            Person clonePerson = (Person) park.clone();//예외처리 해야 함 : clone 리턴타입 object : 형 변환 필요
+            System.out.println("복사된 객체 정보 : " + clonePerson);
+            //System.out.println("park의 주소 : " + park.hashCode());
+            //System.out.println("복제된 객체 주소 : " clonePerson.hashCode());
+            //주소를 넘기는 것 얕은 복제, cloned : 깊은 복제
+            park.setName("박영희");
+            System.out.println("clonePerson");
+
+
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/etc_JG/exception/A05api/lang/obj/Person.java
+++ src/etc_JG/exception/A05api/lang/obj/Person.java
@@ -1,19 +1,77 @@
 package etc_JG.exception.A05api.lang.obj;
 
-public class Person extends Object{//입력하지 않아도 자동생성
+import java.util.Objects;
+
+public class Person implements Cloneable { // Cloneable 사용하기 세팅 //extends Object 입력하지 않아도 자동생성
 //ctrl O 기본 object 값 오버라이딩
 
-    private String name;
-    private int age;
+private String name;
+private int age;
 
     public Person(String name, int age) {
         this.name = name;
         this.age = age;
     }
 
+
+
+    //인강 @@@
+    //toString() : 객체의 정보(필드)를 문자열 형태로 리턴.
+    @Override //부모가 물려준 메서드 내부만 변경 가능 //toString 커스텀
+    public String toString() {
+        return "Person [이름: " +name+ ", 나이" +age+ "]";
+    }
+
+    @Override //o를 드래그하고 shift f6 일괄 이름변경 가능
+    public boolean equals(Object obj) { //equals 마법사로 만듦 //오버라이딩 return, equals(Object ) 변경 불가 : 변수명은(o) 변경가능 // type, 매개변수 개수 증가 XXX
+        if (this == obj) return true; //주소값이 같다면 같은 객체야
+        if (obj instanceof Person) { //obj가 Person type을 갖을 수 있어?
+            Person p = (Person) obj; //obj(부모)person 타입 확인 불가 : person타입 필요 -> instanceof로 걸러준다
+            //object 타입을 person 타입으로 형변환 ->name과 age를 확인하기 위해
+            if(this.name.equals(p.name) && this.age == p.age){ //이름/나이 같은지
+                return true;
+            }
+        }
+        return false; //이외는 false
+    }
+
+
+    //equals를 오버라이딩 할 때는 hashCode도 함께 오버라이딩을 진행해 주시는 것이 좋습니다.
+    //equals를 재 정의한다는 것은 내가 원하는 방식의 동등 비교를 하겠다는 것이기 때문에
+    //hashcode의 값도 그에 맞게 같이 재 정의가 되어야 합니다. (hash 주소를 equals에 맞게 정의)
+    @Override //equal + hashCode 세트메뉴 = 중복개체를 허용하지 않음 //set 집합 : 같은 객체 허용X : hashCode를 통해 걸러내기 때문
+    //equal + hashCode 同时 오버라이딩 必!!! //hash : 같은 중복 피하겠다 : hash 코드 건들지 마셈, equals만 입맞에 맞게 편집
+    public int hashCode() { //1) 주소값 출력 메소드 //요 메소드 유무에 따라 값 달라짐
+        return Objects.hash(name, age); //name, age 맞춰준다 //만약 name만 같아도 된다하면 age 지워도 됨 //hash에 대해 자세히 알지 않아도 됨
+    }
+
+//    public Person() { //생성자
+//        super();
+//    }
+
+    @Override //객체 소멸 직전에 내용 작성 : 사용 권장XXX
+    //생성된 객체가 사라지는 시점에서 자동으로 호출(Garbage Collector 가 실행될 때 자동실행됨)
+    protected void finalize() throws Throwable {
+        System.out.println(this.name + "이가 죽었습니다");
+        super.finalize();
+    }
+
+    //객체 복사 메서드
+    // Cloneable 인터페이스 구현해서 사용해야 함 : 객체 복사해서 쓸 일 어의 없음
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    public void setName(String name) { //name만 변경
+        this.name = name;
+    }
+
     public void personInfo(){
         System.out.println("이름"+name);
         System.out.println("나이"+age+"세");
     }
+
+
 
 }
